94562af3ab384dd84ed55acc0a73f3b197b8dbaf
foluwright <49768316+foluwright@users.noreply.github.com>
2019-11-12 00:47:55 -0500

    Add files via upload

---
+++ FBLA/js/general-scripts.js
@@ -0,0 +1,43 @@
+// Navbar animation on scroll
+$("#mainNav").affix({
+	offset: {
+		top: 100
+	}
+});
+
+// Smooth scroll
+$(function() {
+    $("a[href*=\"#\"]:not([href=\"#\"])").click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $("[name=" + this.hash.slice(1) +']');
+        if (target.length) {
+          $("html, body").animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+  
+  // Animate on scroll
+  function scrollAnimation(element, animation, delay, percentFromTop) {
+      var percent = percentFromTop + "%";
+    var $element = $(element);
+    $element.css("opacity", 0);
+    $element.waypoint(function() {
+      if (typeof(delay) == "undefined") {
+        $element.addClass("animated " + animation);
+        $element.addClass("opacity-fix");
+      } else {
+        setTimeout(function() {
+          $element.addClass("animated " + animation);
+          $element.addClass("opacity-fix");
+        }, delay);
+      }
+    }, { offset: percent});
+}
+
+
+
